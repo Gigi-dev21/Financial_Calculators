025327bdd78b4b2b03c30a0a0384588d7b9e3287
embet-mengistu <131568421+embet-mengistu@users.noreply.github.com>
2025-04-11 11:58:12 -0700

    Finaliazed the calculator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -29,14 +29,23 @@ public class Main {
         total_amount_loan = myscanner.nextDouble();
         System.out.println(total_amount_loan);
 
-        System.out.println("What is the annual interest rate as a percentage.Please do not include %.Eg 7.62,5");
+        System.out.println("What is the annual interest rate as a percentage but do not include %.Eg 7.62, 5");
         annual_interest_rate = myscanner.nextDouble();
+        annual_interest_rate = annual_interest_rate / 100;
         System.out.println(annual_interest_rate);
 
         System.out.println("What is the loan term in Years.Eg 12,34");
         loan_term_in_Years = myscanner.nextInt();
         System.out.println(loan_term_in_Years);
 
+        number_of_monthly_payments=12*loan_term_in_Years;
+        monthly_interest_rate= annual_interest_rate/12;
 
+        monthly_payment = total_amount_loan * (monthly_interest_rate * Math.pow(1 + monthly_interest_rate, number_of_monthly_payments)) /
+                (Math.pow(1 + monthly_interest_rate, number_of_monthly_payments) - 1);
+
+        total_interest=(monthly_payment*number_of_monthly_payments)-total_amount_loan;
+
+        System.out.println("Monthly payment is" + " " + monthly_payment+ "and the total interest is" + " " +total_interest );
     }
 }
